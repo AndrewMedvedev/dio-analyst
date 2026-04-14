58c6b9f875cffa357b48e4bee03cb879247d7f44
andrey baidarka <andrejmedvedev2713@gmail.com>
2026-04-14 10:45:17 +0500

    Добавил семантическое ядро

--- src/seo/agents/subagents/analyst.py
+++ src/seo/agents/subagents/analyst.py
@@ -4,13 +4,9 @@ from typing import Annotated, TypedDict
 
 from langgraph.graph import END, START, StateGraph
 
-from ...core.depends import (
-    gpt_oss_120b,
-    parser_expertise,
-    parser_specialization,
-)
-from ...schemas import ExpertiseSite, SpecializationSite
-from ..prompts import PROMPT_EXPERTISE, PROMPT_SPECIALIZATION
+from ...core.depends import gpt_oss_120b, parser_expertise, parser_sc, parser_specialization
+from ...schemas import ExpertiseSite, SemanticCore, SpecializationSite
+from ..prompts import PROMPT_EXPERTISE, PROMPT_SEMANTIC_CORE, PROMPT_SPECIALIZATION
 from .utils import count_tokens
 
 logger = logging.getLogger(__name__)
@@ -62,32 +58,34 @@ async def get_expertise(state: State) -> dict:
     }
 
 
-"""async def get_semantic_core(state: State) -> dict:
+async def get_semantic_core(state: State) -> dict:
     request = PROMPT_SEMANTIC_CORE.format(
-        data=..., format_instructions=parser_sc.get_format_instructions()
+        data=state["markdown"], format_instructions=parser_sc.get_format_instructions()
     )
-    chain = yandex_gpt | parser_sc
+    chain = gpt_oss_120b | parser_sc
     result: SemanticCore = await chain.ainvoke(request)
     tokens = await count_tokens(request, result.model_dump_json())
-    total_tokens = tokens + state["total_tokens"]
+    total_tokens = tokens + state.get("total_tokens", 0)
     logger.info("Получения семантического ядра компании")
-    total_money = (tokens / 1000 * 0.80) + state["total_money"]
+    total_money = (tokens / 1000 * 0.30) + state.get("total_money", 0)
     return {
         "total_tokens": total_tokens,
         "semantic_core": result.model_dump(),
         "total_money": total_money,
     }
-"""
+
 
 builder = StateGraph(State)
 
 builder.add_node("get_specialization", get_specialization)
 builder.add_node("get_expertise", get_expertise)
-"""builder.add_node("get_semantic_core", get_semantic_core)"""
+builder.add_node("get_semantic_core", get_semantic_core)
 
 builder.add_edge(START, "get_specialization")
 builder.add_edge(START, "get_expertise")
+builder.add_edge(START, "get_semantic_core")
 builder.add_edge("get_specialization", END)
 builder.add_edge("get_expertise", END)
+builder.add_edge("get_semantic_core", END)
 
 agent_analyst = builder.compile()
